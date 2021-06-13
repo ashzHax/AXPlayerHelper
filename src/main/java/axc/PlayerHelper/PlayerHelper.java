0245e9f65f5323814ac5037e011dde53a742fada
Ash <ashkim2001@gmail.com>
2021-06-13 09:25:49 +0900

    initial commit

---
+++ src/main/java/axc/PlayerHelper/PlayerHelper.java
@@ -0,0 +1,16 @@
+package axc.PlayerHelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class PlayerHelper extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
