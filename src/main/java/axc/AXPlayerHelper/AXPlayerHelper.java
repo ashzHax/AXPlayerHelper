0df64104e066385d7faba317da8ef80e8de770ff
Ash <ashkim2001@gmail.com>
2021-06-13 09:36:56 +0900

    event initial commit

--- src/main/java/axc/AXPlayerHelper/AXPlayerHelper.java
+++ src/main/java/axc/AXPlayerHelper/AXPlayerHelper.java
@@ -1,12 +1,13 @@
 package axc.AXPlayerHelper;
 
+import axc.AXPlayerHelper.events.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
 public class AXPlayerHelper extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        this.getServer().getPluginManager().registerEvents(new PlayerDeath(this), this);
     }
 
     @Override
---
+++ src/main/java/axc/AXPlayerHelper/events/PlayerDeath.java
@@ -0,0 +1,15 @@
+package axc.AXPlayerHelper.events;
+
+import axc.AXPlayerHelper.AXPlayerHelper;
+import org.bukkit.event.Listener;
+
+public class PlayerDeath implements Listener {
+
+    private AXPlayerHelper plugin;
+    public PlayerDeath(AXPlayerHelper p) {
+        this.plugin = p;
+    }
+
+
+
+}
